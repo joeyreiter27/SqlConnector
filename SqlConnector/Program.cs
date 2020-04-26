18f2ae35bc9baea20886ea667bdc2113f75d7364
Joe Reiter <joeyreiter27@gmail.com>
2020-04-26 14:25:43 -0400

    link PartNumbers table from sql file TSeries Controller Database to VS Program file. prove it works with console.writeline

--- SqlConnector/Program.cs
+++ SqlConnector/Program.cs
@@ -9,27 +9,30 @@ namespace SqlConnector
         {
 
             // text within quotes taken from connectionstrings.com
-            using(var sqlConnection = new SqlConnection("Server=.;Database=Tseries Controller Database;User Id=sa;Password=Myname02!;"))
+            using(var sqlConnection = new SqlConnection("Server=.;Database=TSeries Controller Database;User Id=sa;Password=Myname02!;"))
             {
                 //opens the database connection
                 sqlConnection.Open();
 
 
 
+                /* Insert command commented out for the time being.... column names will need updated
 
-                //Inserts a new row of values into all specified columns
-                // the Guid functions are used to give the value a generated unique value
+                 //Inserts a new row of values into all specified columns
+                 // the Guid functions are used to give the value a generated unique value
 
-                using (var command = new SqlCommand($"INSERT INTO  dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Username1', 'My first name', 'My last name', 1,'12/12/2020 11:30:30 AM +00:00' )", sqlConnection))
-                {
-                    // Checks to ensure the insert function was executed... result = 1 means it executed
-                    var result = command.ExecuteNonQuery();
-                   
+                 using (var command = new SqlCommand($"INSERT INTO  dbo.Users (id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Username1', 'My first name', 'My last name', 1,'12/12/2020 11:30:30 AM +00:00' )", sqlConnection))
+                 {
+                     // Checks to ensure the insert function was executed... result = 1 means it executed
+                     var result = command.ExecuteNonQuery();
 
 
-                }
+
+                 }
+                 */
+
                 //used to select data from the Users database
-                using (var command = new SqlCommand("SELECT * FROM dbo.Users",sqlConnection))
+                using (var command = new SqlCommand("SELECT * FROM dbo.PartNumbers$", sqlConnection))
                 {
                     //assigns the values of what is read in each cell to the variable reader 
                     // there are multiple other execute options that will give you different options for what values youll return from the table
@@ -40,7 +43,7 @@ namespace SqlConnector
                         {
                             // writes the content of the table in a logical text statement
                             // 
-                            Console.WriteLine($"Username { reader["Username"]}, FirstName: {reader["FirstName"]}, LastName: {reader["LastName"]},IsEnabled: {reader["IsEnabled"]}");
+                            Console.WriteLine($"ABB New Part Number: { reader["ABB New Part Number"]}, SqD New Part Number: {reader["SqD New Part Number"]}, MatchCode: {reader["MatchCode"]},Model: {reader["Model"]}");
                         }
                     }
 
